93c16eb3a375e73ccabbb246ef0dea5db56b9f29
moundu <munenemuondu@gmail.com>
2021-02-05 19:43:20 +0300

    I have done this calculator

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Hello. This is a calculator where it finds the gradient of a line")
+xValue = int(input("Enter your x value:  "))
+yValue = int(input("Enter your y value:  "))
+answer = xValue/yValue
+print("The answer is ",str(answer))
